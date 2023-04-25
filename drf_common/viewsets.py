45c6b9c016ad781bfc599a6bce168a193cf8e498
Ahmed Ibrahim <leondaz@macbook.local>
2023-04-25 21:12:03 +0200

    add partial_update & provide sensible default is no permissions were passed

--- drf_common/viewsets.py
+++ drf_common/viewsets.py
@@ -4,9 +4,11 @@ from rest_framework import viewsets
 
 
 class MultiPermissionViewSet(viewsets.ModelViewSet):
-    permission_classes_create = []
-    permission_classes_update = []
-    permission_classes_destroy = []
+    permission_classes_retrieve = None
+    permission_classes_list = None
+    permission_classes_create = None
+    permission_classes_update = None
+    permission_classes_destroy = None
 
     input_serializer_class = None
     output_serializer_class = None
@@ -19,10 +21,16 @@ class MultiPermissionViewSet(viewsets.ModelViewSet):
         return {
             "create": self.permission_classes_create,
             "update": self.permission_classes_update,
+            "partial_update": self.permission_classes_update,
             "destroy": self.permission_classes_destroy,
-            "retrieve": self.permission_classes,
-            "list": self.permission_classes,
+            "retrieve": self.permission_classes_retrieve,
+            "list": self.permission_classes_list,
         }
 
     def get_permissions(self):
-        return [perm() for perm in self.get_action_perms_map().get(self.action, [])]
+        return [
+            perm()
+            for perm in self.get_action_perms_map().get(
+                self.action, self.permission_classes
+            )
+        ]
