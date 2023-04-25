ee05ac7bf77f192d868a9ad523e70073c634751b
Ahmed Ibrahim <leondaz@macbook.local>
2023-04-25 20:42:47 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name="drf_common",
+    version="1.0",
+    description="Django rest framework common utilities",
+    author="LeOndaz",
+    author_email="ahmeddark369@gmail.com",
+    url="https://github.com/LeOndaz/drf-common",
+)
