06f88d52d541c900ad858ce203ba3110bb76e07a
Ahmed Ibrahim <leondaz@macbook.local>
2023-04-26 05:09:51 +0200

    add alpha version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="django_rest_commons",
-    version="1.0",
+    version="1.0.1",
     description="Django rest framework common utilities",
     author="LeOndaz",
     author_email="ahmeddark369@gmail.com",
