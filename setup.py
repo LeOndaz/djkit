b35115f51f318bfbdb311434f070ede6f329d517
leondaz <ahmed.ibrahim@bld.ai>
2023-11-24 09:17:01 +0200

    update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name="django_rest_commons",
-    version="1.0.2",
+    version="1.0.3",
     description="Django rest framework common utilities",
     author="LeOndaz",
     author_email="ahmeddark369@gmail.com",
