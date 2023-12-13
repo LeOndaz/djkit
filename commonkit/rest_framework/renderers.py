41d6d476b7d38e14f5f791d54d6ccd1cb6707d71
leondaz <ahmed.ibrahim@bld.ai>
2023-12-13 08:11:00 +0200

    reduce nested ifs

--- commonkit/rest_framework/renderers.py
+++ commonkit/rest_framework/renderers.py
@@ -23,16 +23,11 @@ class JSONRenderer(BaseJsonRenderer):
         }
 
     def render(self, data, accepted_media_type=None, renderer_context=None):
-        if renderer_context:
-            response = renderer_context.get("response", None)
-            if response:
-                if response.status_code >= 400:
-                    data = self.render_errors(
-                        data, accepted_media_type, renderer_context
-                    )
-                else:
-                    data = self.render_success(
-                        data, accepted_media_type, renderer_context
-                    )
+        if renderer_context and renderer_context.get("response", None):
+            response = renderer_context["response"]
+            if response.status_code >= 400:
+                data = self.render_errors(data, accepted_media_type, renderer_context)
+            else:
+                data = self.render_success(data, accepted_media_type, renderer_context)
 
         return super().render(data, accepted_media_type, renderer_context)
